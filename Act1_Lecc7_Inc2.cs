53e32c8ce40233bb95f9c6f05bf8cbdd5fc61791
20APG01-AndresPerez <a.perez.5adb@gmail.com>
2026-04-01 11:11:38 -0600

    Add files via upload

---
+++ Act1_Lecc7_Inc2.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--VERIFICACIÓN DE MAYORÍA DE EDAD--");
+        int edad;
+
+        //Enviar informacion al usuario
+        Console.Write("Ingrese su edad: ");
+        edad = Convert.ToInt32(Console.ReadLine());
+
+        //Proceso de verificación
+        if (edad > 18) {
+            Console.WriteLine("Usted es mayor de edad.");
+
+        }
+        else {
+            Console.WriteLine("Usted es menor de edad.");
+        }
+    }
+}
---
+++ Act1_Lecc7_Inc2.psc
@@ -0,0 +1,13 @@
+Algoritmo Act1_Lecc7_Inc2
+	Definir edad Como Entero
+	
+	Escribir "Escriba su edad"
+	Leer edad
+	
+	//Evauluar datos 
+	Si edad >=18 Entonces
+		Escribir "Usted es Mayor de edad"
+	SiNo
+		Escribir "No es mayor de edad"
+	Fin Si
+FinAlgoritmo
