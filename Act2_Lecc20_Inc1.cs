95eb901d2303b7c1d8fd9a702651676dda660df9
20APG01-AndresPerez <a.perez.5adb@gmail.com>
2026-04-01 17:20:00 -0600

    Add files via upload

---
+++ Act2_Lecc20_Inc1.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--CONTRASEÑA--");
+        string contra;
+
+        Console.Write("Digite la contraseña:  ");
+        contra = Console.ReadLine();
+
+        if (contra == "Password123") 
+        {
+            Console.WriteLine("Bienvenido, tiene acceso a su dispositivo");
+        }
+        else
+        {
+            Console.WriteLine("ACCESO DENEGADO");
+        }
+    }
+}
---
+++ Act2_Lecc20_Inc1.psc
@@ -0,0 +1,11 @@
+Algoritmo Act2_Lecc20_Inc1
+	Definir contraseña Como Caracter
+	Escribir "Escriba su Password"
+	Leer contraseña
+	
+	Si contraseña = "Password123" Entonces
+		Escribir "Bienvenido, tiene acceso a su dispositivo"
+	SiNo
+		Escribir "ACCESO DENEGADO"
+	FinSi
+FinAlgoritmo
