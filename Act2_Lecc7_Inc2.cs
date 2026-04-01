0483096475501f3c0a33a70faa4cc69a078d6346
20APG01-AndresPerez <a.perez.5adb@gmail.com>
2026-04-01 13:28:07 -0600

    Add files via upload

---
+++ Act2_Lecc7_Inc2.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--VERFIFICACION DE NOTA");
+        string materia;
+        int nota;
+
+        Console.Write("Digite una materia");
+        materia = Console.ReadLine();
+        Console.Write("Escriba la nota");
+        nota = Convert.ToInt32(Console.ReadLine());
+
+        if (nota > 50 & nota <= 60)
+        {
+            Console.WriteLine("Nota Deficiente");
+        }
+        else if (nota > 61 & nota <= 70){
+            Console.WriteLine("Nota regular");
+        }
+        else if (nota > 71 & nota <= 80)
+        {
+            Console.WriteLine("Nota Buena");
+        }
+        else if (nota > 81 & nota <= 90){
+            Console.WriteLine("Nota muy buena");
+        }
+        else if (nota > 91 & nota <= 100)
+        {
+            Console.WriteLine("Nota excelente");
+        }
+    }
+}
