d7aea79a9c5d7ba1897cf542721a91a721856418
20APG01-AndresPerez <a.perez.5adb@gmail.com>
2026-04-01 18:01:22 -0600

    Add files via upload

---
+++ Act3_Lecc20_Inc2.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--CALCULAR SI EL ALUMNO GANA O PIERDE--");
+        int nota;
+
+        Console.Write("Escriba la nota:  ");
+        nota = Convert.ToInt32(Console.ReadLine());
+
+        if (nota >= 70)
+        {
+            Console.WriteLine("Usted Aprobó el curso");
+        }
+        else
+        {
+            Console.WriteLine("Usted Reprobó el curso");
+        }
+    }
+}
---
+++ Act3_Lecc20_Inc2.psc
@@ -0,0 +1,11 @@
+Algoritmo Act3_Lecc20_Inc2
+	Definir nota Como Entero
+	Escribir "Escriba su nota"
+	Leer nota 
+	
+	Si nota >=70 Entonces
+		Escribir "Usted Aprobo el curso"
+	SiNo
+		Escribir "Usted Reprobo"
+	FinSi
+FinAlgoritmo
